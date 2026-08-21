025934c254e63a145db01f3348286e643ef61459
ndhsfr <indahsafitri@unimugo.ac.id>
2026-08-21 10:09:28 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'user_lpm_unimugo' );
 define( 'DB_PASSWORD', 'LpmUnimugo2026!#' );
 
 /** Database hostname */
-define( 'DB_HOST', 'm81jdl4l6smc87d7sauqhyxy' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
